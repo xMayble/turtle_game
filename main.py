76b048a700e2c98cb6892686ef14e27a4c40c790
Mayble <mehburohit@gmail.com>
2023-11-07 14:10:27 -0500

    added turtle home location

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import turtle as t
-
-turtle_1 = t.Turtle()
-turtle_2 = t.Turtle()
-turtle_3 = t.Turtle()
-turtle_4 = t.Turtle()
-turtle_5 = t.Turtle()
-turtle_6 = t.Turtle()
+from turtle import Turtle, Screen
 
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? Enter a color: ")
+
+
+
+turtle_1 = Turtle(shape="turtle")
+turtle_1.penup()
+turtle_1.goto(x=-240, y=0)
+
+
+
+print(user_bet)
+
+screen.exitonclick()
