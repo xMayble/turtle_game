1869f0ae732c22821cffedeeed2b0f3e19dc72cb
Mayble <mehburohit@gmail.com>
2023-11-09 01:02:15 -0500

    added multi color turtles to diff positions

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+y_positions = [-70, -40, -10, 20, 50, 80]
+
+for turtle_index in range(0,6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-240, y=y_positions[turtle_index])
 
 
 
-turtle_1 = Turtle(shape="turtle")
-turtle_1.penup()
-turtle_1.goto(x=-240, y=0)
 
 
 
